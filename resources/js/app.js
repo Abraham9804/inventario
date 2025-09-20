34eb9ce62d661564db05768b0813c71e893e4652
Abraham9804 <romero9804@hotmail.com>
2025-09-19 19:30:11 -0700

    feat: add datatables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 import './bootstrap';
 import './flowbite.min.js';
 import './fontawesome.js';
+import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
