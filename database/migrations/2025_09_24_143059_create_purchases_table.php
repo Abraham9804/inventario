5d12cb0a720c34a564b57462e713e88e8842308d
Abraham9804 <romero9804@hotmail.com>
2025-09-24 14:33:38 -0700

    feat: create model and migration for purchase

---
+++ database/migrations/2025_09_24_143059_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vourcher_type');
+            $table->string('serie');
+            $table->integer('correlative');
+            $table->timestamp('date');
+            $table->foreignId('purchase_order_id')->constrained()->onDelete('restrict');
+            $table->foreignId('suplier_id')->constrained()->onDelete('restrict');
+            $table->foreignId('warehouse_id')->constrained()->onDelete('restrict');
+            $table->decimal('total', 10, 2)->default(0);
+            $table->string('observation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
