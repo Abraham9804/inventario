afa17ed9325971b8048da874137c8d9439e2c050
Abraham9804 <romero9804@hotmail.com>
2025-10-07 20:46:27 -0700

    feat: Enhance product creation form with category selection and price input

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,8 @@ class ProductController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
+    {   
+        
         return view('admin.products.index');
     }
 
@@ -21,7 +23,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('admin.products.create');
+        $categories = Category::all();
+        return view('admin.products.create', compact('categories'));
     }
 
     /**
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -8,10 +8,25 @@
     ]
 ]">
 
-<x-slot name="action">
-    <a href="{{ route('admin.products.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-        Lista de productos
-    </a>
-</x-slot>
+    <x-slot name="action">
+        <a href="{{ route('admin.products.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+            Lista de productos
+        </a>
+    </x-slot>
+
+    <x-wire-card>
+        <form method="POST" action="{{ route('admin.products.store')}}">
+            @csrf
+            <x-wire-input name="name" label="Nombre" placeholder="Nombre del producto" value="{{old('name')}}"/>
+            <x-wire-textarea name="description" label="Descripción" placeholder="Descripción del producto">{{old('description')}}</x-wire-textarea>
+            <x-wire-input name="price" label="Precio" placeholder="0.00" value="{{old('price')}}" type="number" step="0.01" min="0"/>
+            <x-wire-native-select name="category_id" label="Categoría">
+                <option value="" disabled selected>Seleccione una categoría</option>
+                @foreach($categories as $category)
+                    <option value="{{$category->id}}" selected="{{ old('category_id') == $category->id}}">{{$category->name}}</option>
+                @endforeach
+            </x-wire-native-select>
+        </form>
+    </x-wire-card>
 
 </x-admin-layout>
